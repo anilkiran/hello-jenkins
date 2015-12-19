39dee1eddb6a4d1ba7bca9c82308335532002d8b
dad <sai@sio>
2015-12-18 22:34:48 -0800

    hello world hi

--- test/test.js
+++ test/test.js
@@ -2,12 +2,9 @@ var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
-<<<<<<< HEAD
-  it('respond with hello world', function(done) {
+
+  it('respond with hello world hi', function(done) {
     request(app).get('/').expect('hello world hi', done);
-=======
-  it('respond with hello world-pitta bye', function(done) {
-    request(app).get('/').expect('hello world-pitta bye', done);
->>>>>>> c724a119fac4878092260034c4d8abbc0420d576
+
   });
 });
